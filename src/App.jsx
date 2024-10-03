00e6a7d59dbaa1ca6604096cb63548b9b1469001
Joseph Antony Pierre Avila Foure - Tismart <extjpierre@belcorp.biz>
2024-10-02 23:48:17 -0500

    Add titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import SearchBar from './components/SearchBar'
+import Titulo from './components/Titulo'
 import './App.css'
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <SearchBar></SearchBar>
+      <Titulo></Titulo>
     </>
   )
 }
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,12 @@
+import './Titulo.css'
+import React from 'react'
+
+function Titulo() {
+    return (
+        <div>
+            <h1>Snap Shot</h1>
+        </div>
+    )
+}
+
+export default Titulo
