57607f3782b4330e5b03a61b518f2db254c2e3df
Joseph Antony Pierre Avila Foure - Tismart <extjpierre@belcorp.biz>
2024-10-07 20:30:12 -0500

    detalles

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -13,7 +13,7 @@ export default function Buttons() {
                     <div className="relative w-full">
                         <input type="search" id="search-dropdown" className="rounded-md block p-2.5 w-full text-sm bg-slate-300 dark:placeholder-gray-400 dark:text-black " placeholder="Search..." onChange={(e) =>
                             setNomPage(e.target.value)} />
-                        <button type="submit" className="flex justify-center absolute top-0 end-0 p-2.5 text-sm font-medium h-full w-20 text-white bg-gray-400 rounded-e-lg border border-gray-500 hover:bg-gray-800" onClick={getData}>
+                        <button type="submit" className="flex justify-center absolute top-0 end-0 p-2.5 text-sm font-medium h-full w-20 text-white bg-gray-400 rounded-e-lg border border-gray-500 hover:bg-gray-800">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                             </svg>
